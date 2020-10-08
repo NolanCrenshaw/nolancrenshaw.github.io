8e11182a2ffedc941409c2a663e1143589c438c8
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-10-08 11:13:22 -0400

    replace pre-built component

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Main = () => {
+
+
+// --- Component Render ---
+
+    return (
+        <div className="main__root">
+            <div className="main">
+                <p>Hello World</p>
+            </div>
+        </div>
+    )
+};
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Main from './components/Main';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
