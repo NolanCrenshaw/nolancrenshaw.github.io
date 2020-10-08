0b12dda8266b7d2d7f18e8a2e9460b35b762b513
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-10-08 13:53:10 -0400

    initial product ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './app.css';
 
 const App = () => {
 
@@ -11,6 +11,9 @@ const App = () => {
             <div className="app">
                 <div className="app__textbox">
                     <span>Coming Soon</span>
+                </div>
+                <div className="app__bg-img--attribute">
+                    <span>Photo by <a href="https://unsplash.com/@karsten_wuerth?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Karsten Würth</a> on <a href="https://unsplash.com/s/photos/mountains?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
                 </div>
             </div>
         </div>
