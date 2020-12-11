6bad569e280a17efc263d1fc71cfc61f570e50db
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-10 22:17:43 -0500

    styling pull-down

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import './app.css';
+import React from "react";
+import Main from "./components/Main";
 
 const App = () => {
+  // --- Component Render ---
 
-
-// --- Component Render ---
-
-    return (
-        <div className="app__root">
-            <div className="app">
-                <div className="app__textbox">
-                    <span>Coming Soon</span>
-                </div>
-                <div className="app__bg-img--attribute">
-                    <span>Photo by <a href="https://unsplash.com/@karsten_wuerth?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Karsten Würth</a> on <a href="https://unsplash.com/s/photos/mountains?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="app-container">
+      <Main />
+    </div>
+  );
 };
 export default App;
