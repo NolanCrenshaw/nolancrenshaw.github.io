587975b67e73af6fb300ed08a72cc5f8ca6e0e61
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-14 22:32:50 -0500

    skills added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         className="navbar-container"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: 4, duration: 3 }}
+        transition={{ delay: 3, duration: 3 }}
       >
         <nav>
           <div className="nav-linkbox">
--- src/components/Transition.js
+++ src/components/Transition.js
@@ -5,6 +5,84 @@ const Transition = () => {
     <div className="transition-container">
       <div className="top-trans_grad"></div>
       <div className="bulk-trans_grad"></div>
+      <h1>Proficiencies</h1>
+      <section>
+        <li id="javascript_icon">
+          <img
+            alt="JavaScript"
+            src="https://img.icons8.com/color/96/000000/javascript.png"
+          />
+        </li>
+        <li id="python_icon">
+          <img
+            alt="Python"
+            src="https://img.icons8.com/color/96/000000/python.png"
+          />
+        </li>
+        <li id="nodejs_icon">
+          <img
+            alt=" Node JS"
+            src="https://img.icons8.com/color/96/000000/nodejs.png"
+          />
+        </li>
+        <li id="html5_icon">
+          <img
+            alt="HTML5"
+            src="https://img.icons8.com/color/96/000000/html-5.png"
+          />
+        </li>
+        <li id="css3_icon">
+          <img
+            alt="CSS"
+            src="https://img.icons8.com/color/96/000000/css3.png"
+          />
+        </li>
+        <li id="sass_icon">
+          <img
+            alt="Sass"
+            src="https://img.icons8.com/color/96/000000/sass.png"
+          />
+        </li>
+        <li id="git_icon">
+          <img alt="Git" src="https://img.icons8.com/color/96/000000/git.png" />
+        </li>
+        <li id="docker_icon">
+          <img
+            alt="Docker"
+            src="https://img.icons8.com/color/96/000000/docker.png"
+          />
+        </li>
+        <li id="linux_icon">
+          <img
+            alt="Linux"
+            src="https://img.icons8.com/color/96/000000/linux.png"
+          />
+        </li>
+        <li id="Bash" alt="Bash">
+          <img
+            alt="Bash"
+            src="https://img.icons8.com/color/96/000000/console.png"
+          />
+        </li>
+        <li id="postgresql_icon">
+          <img
+            alt="PostgreSQL"
+            src="https://img.icons8.com/color/96/000000/postgreesql.png"
+          />
+        </li>
+        <li id="react_icon">
+          <img
+            alt="React"
+            src="https://img.icons8.com/color/96/000000/react-native.png"
+          />
+        </li>
+        <li id="redux_icon">
+          <img
+            alt="Redux"
+            src="https://img.icons8.com/color/96/000000/redux.png"
+          />
+        </li>
+      </section>
     </div>
   );
 };
