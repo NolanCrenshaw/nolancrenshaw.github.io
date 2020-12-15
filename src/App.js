1e2518a6cd24bcfe83bb7808bc6a43494c572779
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-14 20:27:58 -0500

    clean up and styling break-point

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from "./components/Main";
 import Loading from "./components/Loading";
 
 const App = () => {
-  const [loaded, setLoaded] = useState(true);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { motion } from "framer-motion";
-
-import ProfileBlock from "./svg_library/ProfileBlock";
 import ProfileTag from "./svg_library/ProfileTag";
 
 const Profile = () => {
   return (
     <div className="profile-container">
-      <motion.div
-        id="profile_pic-container"
-        initial={{ opacity: 0, x: 250, scale: 1 }}
-        animate={{ opacity: 1, x: 0, scale: 1 }}
-        transition={{ delay: 1, duration: 2 }}
-      >
-        <img src="/images/51563575.jpg" />
-      </motion.div>
+      <div id="profile_pic-container">
+        <motion.img
+          initial={{ opacity: 0, x: 250, scale: 1 }}
+          animate={{ opacity: 1, x: 0, scale: 1 }}
+          transition={{ delay: 1, duration: 2 }}
+          src="/images/51563575.jpg"
+        />
+      </div>
       <motion.section
         initial={{ opacity: 0, x: -150, scale: 1 }}
         animate={{ opacity: 1, x: 0, scale: 1 }}
