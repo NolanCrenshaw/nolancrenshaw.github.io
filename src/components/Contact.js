8930ab2282516001f57309e4bed744cc68f32e54
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-13 20:05:11 -0500

    contact fleshout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,24 @@ const Contact = () => {
     <div className="contact-container" id="contact">
       <section>
         <ContactForm />
+        <div className="section-divider" />
+        <div className="contact_infobox">
+          <h2>Schedule a meeting.</h2>
+          <p>I'm available for hire!</p>
+          <p>
+            Please feel free to contact me if you think I might be a good fit
+            with your company. Or perhaps you would like to procure my services
+            as an independent contractor.
+          </p>
+          <p>I am at your disposal.</p>
+          <div />
+          <h2>Let me know what you think.</h2>
+          <p>
+            I'm working hard to show what I can offer. Do you like what you see?
+            Did you experience any bugs or rendering problems? Was this site not
+            optimized for your browser or device? Please give me a heads up!
+          </p>
+        </div>
       </section>
     </div>
   );
