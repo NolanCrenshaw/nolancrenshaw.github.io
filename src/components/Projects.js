dc8e28bd1c99c332043a42ac9251587fc39ae814
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-15 16:02:28 -0500

    projects added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,12 +31,54 @@ const Projects = () => {
         </li>
         <li>
           <h1>Chatter</h1>
+          <img
+            alt="chatter_landing"
+            src="https://nwobucky.s3.us-east-2.amazonaws.com/chatter_capture.jpg"
+          />
           <div className="reveal-box">
             <div>
               <p>
-                A clone of the social media giant, Twitter, Chatter was the
+                A clone of the social media giant "Twitter," Chatter was the
                 result of a group seven-day sprint. It served as an advanced
                 lesson in pair-programming and teamwork.
+              </p>
+              <div>
+                <a href="https://chatter-clone.herokuapp.com/">Live Site</a>
+                <h3>|</h3>
+                <a href="https://github.com/DaedalusG/Chatter">GitHub</a>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h1>Welp</h1>
+          <img
+            alt="welp_landing"
+            src="https://nwobucky.s3.us-east-2.amazonaws.com/welp_capture.jpg"
+          />
+          <div className="reveal-box">
+            <div>
+              <p>
+                A clone of the popular review site "Yelp," Welp was the product
+                of a group seven-day sprint. It was built using Pug files with
+                an ExpressJS backend.
+              </p>
+              <div>
+                <a href="https://welpeat.herokuapp.com/">Live Site</a>
+                <h3>|</h3>
+                <a href="https://github.com/sambrady0652/yelp-clone">GitHub</a>
+              </div>
+            </div>
+          </div>
+        </li>
+        {/* <li>
+          <h1>Go Boating!</h1>
+          <div className="reveal-box">
+            <div>
+              <p>
+                A passion project of mine built with React, Redux, and Flask. Go
+                Boating! is a social site that allows users to organize river
+                trips.
               </p>
               <div>
                 <a href="">Live Site</a>
@@ -45,17 +87,7 @@ const Projects = () => {
               </div>
             </div>
           </div>
-        </li>
-        <li>
-          <h1>Go Boating!</h1>
-          <div className="reveal-box">
-            <div>
-              <a href="">GitHub</a>
-              <h3>|</h3>
-              <a href="">Live Site</a>
-            </div>
-          </div>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
