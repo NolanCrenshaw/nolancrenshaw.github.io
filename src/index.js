b2e5778c48a91b668d814aa64996c18d3e6ff1ba
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-23 11:54:41 -0500

    tag manager pkg init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TagManager from "react-gtm-module";
 
 import "./styles/index.css";
 import App from "./App";
 
+// ~~ Google Tag Manager Init
+const tagManagerArgs = {
+  gtmId: "GTM-TSTQJ4X",
+};
+TagManager.initialize(tagManagerArgs);
+
+// ~~ Core Render ~~
 ReactDOM.render(
   <React.StrictMode>
     <App />
