0569b4b992de940b3bd5239a51401e9032ebe534
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-01-28 21:19:46 -0500

    project card styling change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
@@ -18,66 +18,83 @@ const Projects = () => {
       <h4 ref={ref}>My Work.</h4>
       <ul>
         <li>
-          <h1>New Wing Order</h1>
-          <img
-            alt="nwo_landing"
-            src="https://nwobucky.s3.us-east-2.amazonaws.com/landing_capture.jpg"
-          />
-          <div className="reveal-box">
-            <div>
-              <p>
-                Built in React with a Flask backend, New Wing Order is food
-                truck website with Admin controls that allow for Location and
-                Menu updates.
-              </p>
+          <div className="title-box">
+            <h1>New Wing Order</h1>
+            <div className="title-shape_A" />
+          </div>
+          <div className="img-box">
+            <img
+              alt="nwo_landing"
+              src="https://nwobucky.s3.us-east-2.amazonaws.com/landing_capture.jpg"
+            />
+            <div className="reveal-box">
               <div>
-                <a href="https://newwingorder.herokuapp.com/">Live Site</a>
-                <h3>|</h3>
-                <a href="https://github.com/NolanCrenshaw/new-wing-order">
-                  GitHub
-                </a>
+                <p>
+                  Built in React with a Flask backend, New Wing Order is food
+                  truck website with Admin controls that allow for Location and
+                  Menu updates.
+                </p>
+                <div>
+                  <a href="https://newwingorder.herokuapp.com/">Live Site</a>
+                  <h3>|</h3>
+                  <a href="https://github.com/NolanCrenshaw/new-wing-order">
+                    GitHub
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </li>
         <li>
-          <h1>Chatter</h1>
-          <img
-            alt="chatter_landing"
-            src="https://nwobucky.s3.us-east-2.amazonaws.com/chatter_capture.jpg"
-          />
-          <div className="reveal-box">
-            <div>
-              <p>
-                A clone of the social media giant "Twitter," Chatter was the
-                result of a group seven-day sprint. It served as an advanced
-                lesson in pair-programming and teamwork.
-              </p>
+          <div className="title-box">
+            <h1>Chatter</h1>
+            <div className="title-shape_A" />
+          </div>
+          <div className="img-box">
+            <img
+              alt="chatter_landing"
+              src="https://nwobucky.s3.us-east-2.amazonaws.com/chatter_capture.jpg"
+            />
+            <div className="reveal-box">
               <div>
-                <a href="https://chatter-clone.herokuapp.com/">Live Site</a>
-                <h3>|</h3>
-                <a href="https://github.com/DaedalusG/Chatter">GitHub</a>
+                <p>
+                  A clone of the social media giant "Twitter," Chatter was the
+                  result of a group seven-day sprint. It served as an advanced
+                  lesson in pair-programming and teamwork.
+                </p>
+                <div>
+                  <a href="https://chatter-clone.herokuapp.com/">Live Site</a>
+                  <h3>|</h3>
+                  <a href="https://github.com/DaedalusG/Chatter">GitHub</a>
+                </div>
               </div>
             </div>
           </div>
         </li>
         <li>
-          <h1>Welp</h1>
-          <img
-            alt="welp_landing"
-            src="https://nwobucky.s3.us-east-2.amazonaws.com/welp_capture.jpg"
-          />
-          <div className="reveal-box">
-            <div>
-              <p>
-                A clone of the popular review site "Yelp," Welp was the product
-                of a group seven-day sprint. It was built using Pug files with
-                an ExpressJS backend.
-              </p>
+          <div className="title-box">
+            <h1>Welp</h1>
+            <div className="title-shape_A" />
+          </div>
+          <div className="img-box">
+            <img
+              alt="welp_landing"
+              src="https://nwobucky.s3.us-east-2.amazonaws.com/welp_capture.jpg"
+            />
+            <div className="reveal-box">
               <div>
-                <a href="https://welpeat.herokuapp.com/">Live Site</a>
-                <h3>|</h3>
-                <a href="https://github.com/sambrady0652/yelp-clone">GitHub</a>
+                <p>
+                  A clone of the popular review site "Yelp," Welp was the
+                  product of a group seven-day sprint. It was built using Pug
+                  files with an ExpressJS backend.
+                </p>
+                <div>
+                  <a href="https://welpeat.herokuapp.com/">Live Site</a>
+                  <h3>|</h3>
+                  <a href="https://github.com/sambrady0652/yelp-clone">
+                    GitHub
+                  </a>
+                </div>
               </div>
             </div>
           </div>
