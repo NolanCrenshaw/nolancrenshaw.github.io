872bab1b1ca94660e053d678cede1aec6b2b2fa7
NolanCrenshaw <nolan.crenshaw@gmail.com>
2021-02-13 18:34:49 -0500

    viget work

--- src/components/uniques/Viget.js
+++ src/components/uniques/Viget.js
@@ -4,6 +4,15 @@ const Viget = () => {
   return (
     <div className="viget-container unique">
       <h1>Hello Viget</h1>
+      <div className="logo-container">
+        <div className="elipse-container">
+          <div id="elipse_one" />
+        </div>
+        <div className="elipse-container">
+          <div id="elipse_two" />
+        </div>
+        <span>viget</span>
+      </div>
       <p>Here we go.</p>
     </div>
   );
